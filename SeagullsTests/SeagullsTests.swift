9912a8ec90127cab969bd5fcc5c566239a91cc2d
drasticman <andy.bader@gmail.com>
2026-02-04 15:36:31 -0500

    Initial Commit

---
+++ SeagullsTests/SeagullsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SeagullsTests.swift
+//  SeagullsTests
+//
+//  Created by Andy Bader on 2/4/26.
+//
+
+import Testing
+@testable import Seagulls
+
+struct SeagullsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
